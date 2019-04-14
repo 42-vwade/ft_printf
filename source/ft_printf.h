e48016ac7c8fbc61ec016d6782a2e860d8f8c5a1
Victor Wade <viwade@student.42.us.org>
2019-04-14 03:48:56 -0700

    added . for printf

--- source/ft_printf.h
+++ source/ft_printf.h
@@ -6,14 +6,15 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 17:15:19 by viwade            #+#    #+#             */
-/*   Updated: 2019/04/14 02:44:08 by viwade           ###   ########.fr       */
+/*   Updated: 2019/04/14 03:47:27 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <stdarg.h>
 # include "../libft/libft.h"
 # include "ft_printf_structs.h"
+
+int32_t get_params(t_format *object, t_param *set);
 
 #endif
