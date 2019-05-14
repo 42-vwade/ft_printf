d5fef41ed581006c67fac789e1c2d613b253283c
Victor Wade <viwade@student.42.us.org>
2019-05-14 04:14:16 -0700

    added . for printf

--- source/conversion/integer_operation.c
+++ source/conversion/integer_operation.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 06:01:13 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/14 00:32:17 by viwade           ###   ########.fr       */
+/*   Updated: 2019/05/14 01:40:36 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,27 @@ int		parse_i(va_list args, t_format *o)
 	len = ft_intlen(num);
 	if (o->p.width && len < o->p.width)
 		o->count += pad_i(ABS(o->p.width - len) - (num < 0), o->p.flags & 1, 0);
-	ft_putnbr(num);
+	ft_putnbr((int)num);
 	return (len + (num < 0));
+}
+
+static void
+	put_uint(unsigned long long n)
+{
+	if (n >= 10)
+		put_uint(n / 10);
+	write(1, (char[1]){48 + n % 10}, 1);
 }
 
 int		parse_u(va_list args, t_format *o)
 {
 	int		num;
+	size_t	len;
 
-	num = (int)va_arg(args, int);
-	if ((0))
-		num = o->count;
-	return (ft_intlen(num));
+	num = (unsigned int)va_arg(args, int);
+	len = ft_intlen(num);
+	if (o->p.width && len < o->p.width)
+		o->count += pad_i(ABS(o->p.width - len) - (num < 0), o->p.flags & 1, 0);
+	put_uint((unsigned)num);
+	return (len + (num < 0));
 }
