65a5dea927247aeb74af0010c4b00e76acaeb204
Victor Wade <viwade@student.42.us.org>
2019-05-14 06:17:38 -0700

    added . for printf

--- source/conversion/hex_operation.c
+++ source/conversion/hex_operation.c
@@ -6,11 +6,16 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 06:01:13 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/13 17:35:02 by viwade           ###   ########.fr       */
+/*   Updated: 2019/05/14 06:16:55 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+#define PLUS(n) ((n) & (1u << 0))
+#define MINUS(n) ((n) & (1u << 1))
+#define ZERO(n) ((n) & (1u << 2))
+#define HASH(n) ((n) & (1u << 3))
+#define SPACE(n) ((n) & (1u << 4))
 
 /*
 **		OCTAL / HEX
@@ -18,17 +23,24 @@
 
 int		parse_o(va_list args, t_format *o)
 {
-	int		num;
+	unsigned int		num;
+	char	*str;
 
-	num = (int)va_arg(args, int);
-	if ((0))
-		num = o->count;
-	return (ft_intlen(num));
+	num = (unsigned)va_arg(args, int);
+	if (ft_isuppercase(o->str[0]))
+		ft_strcapitalize(str = ft_itoa_base(num, 8));
+	else
+		str = ft_itoa_base(num, 8);
+	if (HASH(o->p.flags) && (num && o->p.precision))
+		str = ft_strjoin_free("0", str);
+	num = write(1, str, ft_strlen(str));
+	free(str);
+	return (num);
 }
 
 int		parse_hex(va_list args, t_format *o)
 {
-	int		num;
+	long long	num;
 	char	*str;
 
 	num = (int)va_arg(args, int);
@@ -36,6 +48,7 @@ int		parse_hex(va_list args, t_format *o)
 		ft_strcapitalize(str = ft_itoa_base(num, 16));
 	else
 		str = ft_itoa_base(num, 16);
+	str = ft_strjoin_free("0x", str);
 	num = write(1, str, ft_strlen(str));
 	free(str);
 	return (num);
