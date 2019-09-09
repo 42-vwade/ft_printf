b2920daaefde0017cb3eded23d5f0ea60a3b0668
Victor Wade <viwade@student.42.us.org>
2019-09-09 00:38:01 -0700

    added . for printf

--- source/ft_printf.c
+++ source/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 19:36:03 by viwade            #+#    #+#             */
-/*   Updated: 2019/09/05 00:16:55 by viwade           ###   ########.fr       */
+/*   Updated: 2019/09/09 00:37:55 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int
 	int		length[1];
 
 	va_start(ap, format);
-	length[0] = ft_vprintf(format, ap) || 0;
+	length[0] = ft_vprintf(format, ap);
 	va_end(ap);
 	return (length[0]);
 }
